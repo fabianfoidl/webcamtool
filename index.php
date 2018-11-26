1e663a43201355fe23b78e069ca91823ffe31a27
fabianfoidl <37536196+fabianfoidl@users.noreply.github.com>
2018-11-26 08:58:56 +0100

    initial commit

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		
+		<title>Webcamtool</title>
+		
+		<link rel="stylesheet" href="css/bootstrap.css">
+		
+		<script src="js/jquery-3.3.1.js"></script>
+
+		<script src="js/bootstrap.js"></script>
+		
+	</head>
+	<body>
+	
+		<p>Lorem ipsum</p>
+		<script>$("p").fadeOut()</script>
+		
+	</body>
+</html>
